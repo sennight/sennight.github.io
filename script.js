6bf1ee65812285102b462f75e370800d293069ca
sennight <44407261+sennight@users.noreply.github.com>
2022-03-10 22:19:43 -0500

    initial commit
    o wo???

---
+++ script.js
@@ -0,0 +1,9 @@
+const message = 'Hello this is a thing' // Try edit me
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+// Log to console
+console.log(message)
+
+// Try other templates: Project -> New
